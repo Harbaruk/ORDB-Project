c6ac1e76d8ffbe06a853c39fbd69a6cf5db55f0e
hryharb <hryhoriyharbaruk@gmail.com>
2018-07-11 17:06:31 +0300

    Task status

---
+++ Starter/Starter.Commin/DomainTaskStatus/DomainTaskStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Starter.Common.DomainTaskStatus
+{
+    public class DomainTaskStatus
+    {
+        private ErrorCollection _errorCollection = new ErrorCollection();
+
+        public ErrorCollection ErrorCollection => _errorCollection;
+
+        public bool HasErrors => _errorCollection.HasErrors;
+
+        public void AddError(string key, string error) => _errorCollection.AddError(key, error);
+
+        public void AddUnkeyedError(string error) => AddError("general", error);
+    }
+}
---
+++ Starter/Starter.Commin/DomainTaskStatus/ErrorCollection.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Starter.Common.DomainTaskStatus
+{
+    public sealed class ErrorCollection
+    {
+        [JsonProperty("errors")]
+        private Dictionary<string, List<string>> _errors { get; set; } = new Dictionary<string, List<string>>();
+
+        [JsonIgnore]
+        public bool HasErrors => _errors.Any();
+
+        public void AddError(string key, string error)
+        {
+            if (!_errors.TryGetValue(key, out List<string> errors))
+            {
+                errors = new List<string>();
+                _errors[key] = errors;
+            }
+            errors.Add(error);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var key in _errors.Keys)
+            {
+                sb.Append($@" > ""{key}""").AppendLine();
+                foreach (var error in _errors[key])
+                {
+                    sb.Append($@"   > ""{error}""").AppendLine();
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
